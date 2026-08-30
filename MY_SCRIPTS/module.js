d9c29c172a5e0548eb6a3b538cc5f39ad29e1c75
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-08-30 20:31:12 +0330

    ROUTES

--- MY_SCRIPTS/module.js
+++ MY_SCRIPTS/module.js
@@ -8,9 +8,18 @@ app.factory("SharedData", function () {
   return { value: 0 }
 })
 
+// APP CONFIG ===========================================================
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider.when("/List", {
     templateUrl: "/list.html",
     controller: "ListController"
+  })
+  $routeProvider.when("/AddNewPerson", {
+    templateUrl: "/addPerson.html",
+    controller: "AddPersonController"
+  })
+  $routeProvider.when("/EditPerson", {
+    templateUrl: "/editPerson.html",
+    controller: "EditPersonController"
   })
 }])
