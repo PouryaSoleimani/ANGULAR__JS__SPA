8ef1c1a0e8da9b95e1196843c9eab78dbf8fc14f
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-04 16:10:25 +0330

    ANGULAR__HTTP__GETALL

--- Controllers/AllUsersController.js
+++ Controllers/AllUsersController.js
@@ -1,8 +1,15 @@
 app.controller('AllPeopleController',
   function ($scope, $location, PersonService, SharedData) {
+      
     var list = PersonService.getAllPerson()
     list.then(function (result) {
       console.log('result =>', result)
       $scope.people = result.data;
     })
+
+    $scope.userDeleteHandler = function (ID) {
+      console.log({ ID })
+      const newList = $scope.people.filter((item) => item.id !== ID)
+      $scope.people = newList
+    }
   }) 
