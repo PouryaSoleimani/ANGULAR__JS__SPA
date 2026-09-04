a1a30d4b01a11f17a60a1593197d2f41e74e7044
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-04 12:01:41 +0330

    ANGULAR__JS__SPA

--- MY_SCRIPTS/module.js
+++ MY_SCRIPTS/module.js
@@ -1,29 +1,17 @@
-var app = angular.module('PersonApp', ["ngRoute"])
+var app = angular.module('PersonApp', ["ngRoute"]) // DEPENDENCIES => NG_ROUTE
 
-function ListController($scope) {
+function ListController($scope) { }
 
-}
-
+// APP.FACTORY ==========================================================
 app.factory("SharedData", function () {
   return { value: 0 }
 })
 
 // APP CONFIG ===========================================================
 app.config(["$routeProvider", function ($routeProvider) {
-  $routeProvider.when("/", {
-    templateUrl: "/list.html",
-    controller: "ListController"
-  })
-  $routeProvider.when("/List", {
-    templateUrl: "/list.html",
-    controller: "ListController"
-  })
-  $routeProvider.when("/AddNewPerson", {
-    templateUrl: "/addPerson.html",
-    controller: "AddPersonController"
-  })
-  $routeProvider.when("/EditPerson", {
-    templateUrl: "/editPerson.html",
-    controller: "EditPersonController"
-  })
+  $routeProvider.when("/", { templateUrl: "/list.html", controller: "ListController" })
+  $routeProvider.when("/List", { templateUrl: "/list.html", controller: "ListController" })
+  $routeProvider.when("/AddNewPerson", { templateUrl: "/addPerson.html", controller: "AddPersonController" })
+  $routeProvider.when("/EditPerson", { templateUrl: "/editPerson.html", controller: "EditPersonController" })
+  $routeProvider.when("/AllPeople", { templateUrl: "/allPeople.html", controller: "AllPeopleController" })
 }])
