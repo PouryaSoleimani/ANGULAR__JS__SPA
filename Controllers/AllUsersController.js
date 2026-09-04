0cfcbd0ee1039397e47f8b32c52c66984d55787c
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-04 18:42:36 +0330

    ANGULAR__HTTP

--- Controllers/AllUsersController.js
+++ Controllers/AllUsersController.js
@@ -1,6 +1,6 @@
 app.controller('AllPeopleController',
   function ($scope, $location, PersonService, SharedData) {
-      
+
     var list = PersonService.getAllPerson()
     list.then(function (result) {
       console.log('result =>', result)
