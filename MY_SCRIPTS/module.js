97e91604b2b754cc26bb76de84b49ba3629e6d89
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-04 12:12:01 +0330

    REVIEW

--- MY_SCRIPTS/module.js
+++ MY_SCRIPTS/module.js
@@ -9,9 +9,24 @@ app.factory("SharedData", function () {
 
 // APP CONFIG ===========================================================
 app.config(["$routeProvider", function ($routeProvider) {
-  $routeProvider.when("/", { templateUrl: "/list.html", controller: "ListController" })
-  $routeProvider.when("/List", { templateUrl: "/list.html", controller: "ListController" })
-  $routeProvider.when("/AddNewPerson", { templateUrl: "/addPerson.html", controller: "AddPersonController" })
-  $routeProvider.when("/EditPerson", { templateUrl: "/editPerson.html", controller: "EditPersonController" })
-  $routeProvider.when("/AllPeople", { templateUrl: "/allPeople.html", controller: "AllPeopleController" })
+  $routeProvider.when("/", {
+    templateUrl: "/list.html",
+    controller: "ListController"
+  })
+  $routeProvider.when("/List", {
+    templateUrl: "/list.html",
+    controller: "ListController"
+  })
+  $routeProvider.when("/AddNewPerson", {
+    templateUrl: "/addPerson.html",
+    controller: "AddPersonController"
+  })
+  $routeProvider.when("/EditPerson", {
+    templateUrl: "/editPerson.html",
+    controller: "EditPersonController"
+  })
+  $routeProvider.when("/AllPeople", {
+    templateUrl: "/allPeople.html",
+    controller: "AllPeopleController"
+  })
 }])
