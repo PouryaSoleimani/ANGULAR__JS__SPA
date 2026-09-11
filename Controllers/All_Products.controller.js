6f293bc7f9a4e45beddbe45160a10afe57ccaf1d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-11 18:35:58 +0330

    PRODUCTS

--- Controllers/All_Products.controller.js
+++ Controllers/All_Products.controller.js
@@ -1,8 +1,12 @@
 app.controller('AllProductsController', function ($scope, ProductService) {
+  // GET ALL PRODUCTS
   var productsList = ProductService.getAllProducts()
   productsList.then(res => {
     $scope.products = res.data
     console.log({ products: res.data })
     return res
   })
+
+  
+
 })
