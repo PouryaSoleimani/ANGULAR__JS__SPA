7089a213dfb28f78e41def9cc5379ca74a424e37
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-11 20:55:13 +0330

    PRODUCTS

--- Modules/App.module.js
+++ Modules/App.module.js
@@ -1,37 +1,40 @@
 // DEPENDENCIES FOR MODULE => NG_ROUTE
-var app = angular.module('UsersApp', ["ngRoute"])
+var app = angular.module("UsersApp", ["ngRoute"]);
 
 // APP.FACTORY ==========================================================
 app.factory("SharedData", function () {
-  return { value: 0, }
-})
+  return { value: 0 };
+});
 
 // APP CONFIG | ROUTE_PROVIDER ===========================================================
-app.config(["$routeProvider", function ($routeProvider) {
-  // USERS ROUTES
-  $routeProvider.when("/", {
-    templateUrl: "/AllUsers.html",
-    controller: "AllPeopleController"
-  })
-  $routeProvider.when("/AllUsers", {
-    templateUrl: "/AllUsers.html",
-    controller: "AllPeopleController"
-  })
-  $routeProvider.when("/SearchUser", {
-    templateUrl: "/SearchUser.html",
-    controller: "SingleUserController"
-  })
-  $routeProvider.when("/AddNewPerson", {
-    templateUrl: "/AddUser.html",
-    controller: "AddPersonController"
-  })
-  // PRODUCTS ROUTES
-  $routeProvider.when('/AllProducts', {
-    templateUrl: '/AllProducts.html',
-    controller: 'AllProductsController'
-  })
-  $routeProvider.when('/SearchProduct', {
-    templateUrl: '/SearchProduct.html',
-    controller: "SearchProductController"
-  })
-}])
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
+    // USERS ROUTES
+    $routeProvider.when("/", {
+      templateUrl: "/AllUsers.html",
+      controller: "AllPeopleController",
+    });
+    $routeProvider.when("/AllUsers", {
+      templateUrl: "/AllUsers.html",
+      controller: "AllPeopleController",
+    });
+    $routeProvider.when("/SearchUser", {
+      templateUrl: "/SearchUser.html",
+      controller: "SingleUserController",
+    });
+    $routeProvider.when("/AddNewPerson", {
+      templateUrl: "/AddUser.html",
+      controller: "AddPersonController",
+    });
+    // PRODUCTS ROUTES
+    $routeProvider.when("/AllProducts", {
+      templateUrl: "/AllProducts.html",
+      controller: "AllProductsController",
+    });
+    $routeProvider.when("/SearchProduct", {
+      templateUrl: "/SearchProduct.html",
+      controller: "SearchProductController",
+    });
+  },
+]);
