f749d2e37377003b65354e37803e37db9bb51432
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-11 13:14:37 +0330

    ADD__USERS

--- Controllers/Single_User.controller.js
+++ Controllers/Single_User.controller.js
@@ -2,7 +2,11 @@ app.controller('SingleUserController', function ($scope, PersonService) {
 
   // GET ALL USERS
   var list = PersonService.getAllPeople()
-  $scope.people = list;
+  list.then(res => {
+    console.log('result =>', res.data)
+    $scope.people = res.data;
+  })
+
   // SEARCH HANDLER
   $scope.searchHandler = function () {
     $scope.user = $scope.people.find(item => item.id == $scope.ID)
