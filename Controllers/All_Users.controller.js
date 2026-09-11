96c6c5a02526b3cff83ffe8b00c512e7e6877be0
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2026-09-11 09:25:09 +0330

    EDIT___USERS:

--- Controllers/All_Users.controller.js
+++ Controllers/All_Users.controller.js
@@ -1,6 +1,9 @@
 app.controller('AllPeopleController',
   function ($scope, $location, PersonService, SharedData) {
-
+    $scope.vm = {
+      editModal: document.querySelector('.edit_person_modal'),
+      selectedUser: {}
+    }
     var list = PersonService.getAllPerson()
     list.then(function (result) {
       console.log('%c ALL USERS =>', "color:yellow", result.data)
@@ -11,5 +14,19 @@ app.controller('AllPeopleController',
       console.log({ ID })
       const newList = $scope.people.filter((item) => item.id !== ID)
       $scope.people = newList
+    }
+
+    $scope.openModalHandler = function (user) {
+      $scope.vm.editModal.classList.add('active')
+      $scope.vm.selectedUser = user
+    }
+
+    $scope.closeModalHandler = function () {
+      $scope.vm.editModal.classList.remove('active')
+    }
+
+    $scope.submitEditFormHandler = function () {
+      console.log('SUBMIT')
+      $scope.closeModalHandler()
     }
   }) 
